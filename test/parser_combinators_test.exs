5cc7c82526f7a449150b720a3dd863b92ff32381
Carlos Henrique <ch.henriquevinha@gmail.com>
2026-01-28 18:16:37 -0300

    add test letter

--- test/parser_combinators_test.exs
+++ test/parser_combinators_test.exs
@@ -15,4 +15,16 @@ defmodule ParserCombinatorsTest do
   test "digit/s234" do 
     assert %{error: "digit not found", input: "s234"} = P.digit("s234")
   end
+
+  test "letter/a" do
+    assert {:ok, "a", ""} = P.letter("a")
+  end
+
+  test "letter/as2" do 
+    assert {:ok, "a", "s2"} = P.letter("as2")
+  end
+
+  test "letter/234" do 
+    assert %{error: "letter not found", input: "234"} = P.letter("234")
+  end
 end
